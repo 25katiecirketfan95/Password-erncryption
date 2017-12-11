b38b5e8ddb6dcfa6a8d4261e6f9a76315ff76b7c
Katharina Rux <30776551+25katiecricketfan95@users.noreply.github.com>
2017-12-11 17:38:24 +0100

    Add files via upload

--- utils.js
+++ utils.js
@@ -1,10 +1,11 @@
 var crypto = require('crypto');
 var prompt = require('prompt');
 var fs = require('fs');
-
-
+var stringUtils = require('./stringUtils');
 
 prompt.start();
+
+
 
 function account() {
 
@@ -32,11 +33,62 @@ function newAccount() {
 
 	prompt.get(["username", "password"], function (err, result) {
 
-		var hash = generateHash(result.password)
+		var hash = generateHash(result.clapassword)
 		var credentials = {
 			password: hash,
 			username: result.username
 		}
+
+		if (credentials.username.length >= 8) {
+
+		} else {
+			console.log('You need at least eight characters! Make sure you have at least one upper case, one lower case and one number in your username!')
+		}
+
+
+		for (var i = 0; i < credentials.username.length; i++); {
+			var char = credentials.username.charAt(i);
+
+
+			var success = stringUtils.isUpper(char);
+
+			if ((isUppercase)) {
+				console.log("At least one lower case Character and one Number is required!");
+			}
+			else if (!(isLowercase)) {
+				console.log("At least one upper case Character and Number required!");
+			}
+			else if (!(isNumber)) {
+				console.log("At least one uppercase and lower case letter required!");
+			} else {
+
+			}
+		}
+
+		/* 
+		if (isUppercase) {
+			goto nextInteger
+		} else {
+			goto isLowercase
+		}
+
+		if (isLowercase) {
+			goto nextInteger
+		} else {
+			goto isNumber
+		}
+
+		if (isNumber) {
+			goto nextInteger
+		} else if (lastInteger) {
+			break
+		}
+		-> for the for- loop as several if- else- Bedingungen in one, could also use if else if Bedingung
+			plus nextInteger needs to be defined
+		 */
+
+
+
 
 		var json = fs.readFileSync("./data.json");
 		var credentialsArray = JSON.parse(json);
@@ -67,14 +119,18 @@ function login() {
 		}
 
 		var json = fs.readFileSync("./data.json");
-		var registeredCredentials = JSON.parse(json);
+		var credentialsArray = JSON.parse(json);
 
-		if (credentials.password == registeredCredentials.password) {
-			console.log('Login sucessful')
+		var exsistingPassword = credentialsArray.filter(function (item) {
+			return item.password == credentials.password;
+		});
+		if (exsistingPassword.length > 0) {
+			var json = JSON.stringify(credentialsArray, null, 3);
+			fs.writeFileSync("./data.json", json);
 		} else {
-			console.log('error')
+			console.log('Try Again')
 		}
-	}); //todo: Array to search for passwords in data.json
+	});
 }
 
 function generateHash(text) {
