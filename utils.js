ac188fb2e6a78cf2f9b2dad540d58d499cab5384
Katharina <katharina.rux@lustitia.de>
2017-12-18 16:35:00 +0100

    Utils updated

--- utils.js
+++ utils.js
@@ -1,41 +1,70 @@
 var crypto = require('crypto');
 var prompt = require('prompt');
 var fs = require('fs');
-
-
+var stringUtils = require('./stringUtils');
 
 prompt.start();
 
-function account() {
+
+
+function requestIfUserHasAnAccount() {
 
 	var schema = {
 		properties: {
 			input: {
-				description: 'Do you already have an account?',
+				description: 'Do you already have an account (Y/N)?',
 			}
 		}
 	};
 
-
 	prompt.get(schema, function (err, result) {
 		console.log(result.input);
-		if ('Yes' == result.input) {
+		if ('Y' == result.input.toUpperCase()) {
 			login();
-		} else {
+		} else if ('N' == result.input.toUpperCase()){
 			newAccount();
+		}
+		else {
+			console.error('Please press Y or N.');
+			requestIfUserHasAnAccount();
 		}
 	});
 }
 
-// New Login Details
-function newAccount() {
+function register() {
 
 	prompt.get(["username", "password"], function (err, result) {
 
-		var hash = generateHash(result.password)
+		var hash = generateHash(result.clapassword)
 		var credentials = {
 			password: hash,
 			username: result.username
+		}
+
+		if (credentials.username.length >= 8) {
+			usernameCharacters()
+		} else {
+			console.log('You need at least eight characters! Make sure you have at least one upper case, one lower case and one number in your username!')
+		}
+
+		function usernameCharacters()
+		for (var i = 0; i < credentials.username.length; i++); {
+			var char = credentials.username.charAt(i);
+
+
+			var success = stringUtils.isUpper(char);
+
+			if ((isUppercase)) {
+				console.log("At least one lower case Character and one Number is required!");
+			}
+			else if (!(isLowercase)) {
+				console.log("At least one upper case Character and Number required!");
+			}
+			else if (!(isNumber)) {
+				console.log("At least one uppercase and lower case letter required!");
+			} else {
+
+			}
 		}
 
 		var json = fs.readFileSync("./data.json");
@@ -67,14 +96,18 @@ function login() {
 		}
 
 		var json = fs.readFileSync("./data.json");
-		var registeredCredentials = JSON.parse(json);
+		var credentialsArray = JSON.parse(json);
 
-		if (credentials.password == registeredCredentials.password) {
-			console.log('Login sucessful')
+		var exsistingPassword = credentialsArray.filter(function (item) {
+			return item.password == credentials.password;
+		});
+		if (exsistingPassword.length > 0) {
+			var json = JSON.stringify(credentialsArray, null, 3);
+			fs.writeFileSync("./data.json", json);
 		} else {
-			console.log('error')
+			console.log('Try Again')
 		}
-	}); //todo: Array to search for passwords in data.json
+	});
 }
 
 function generateHash(text) {
